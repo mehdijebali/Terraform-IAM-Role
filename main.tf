c7d1128d295e48e85f70a117811d3097701ac8c5
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-12-23 09:37:02 +0100

    Configure instance module

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
 provider "aws" {
   region     = var.AWS_REGION
 }
+
+module "instance" {
+  source = "./modules/instance"
+
+  KEY_NAME = var.KEY_NAME
+  PATH_TO_PUBLIC_KEY = var.PATH_TO_PUBLIC_KEY
+  SG_VPC_ID = data.aws_vpc.default.id
+  SG_NAME = var.SG_NAME
+  SG_DESCRIPTION = var.SG_DESCRIPTION
+  AMI_ID = var.AMI_ID
+  INSTANCE_TYPE = var.INSTANCE_TYPE
+  INSTANCE_NAME = var.INSTANCE_NAME
+  AVAILABILITY_ZONE = var.AVAILABILITY_ZONE
+  INSTANCE_PROFILE = aws_iam_instance_profile.s3-levelupbucket-role-instanceprofile
+}
