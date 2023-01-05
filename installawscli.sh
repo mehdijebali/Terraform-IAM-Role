49b8db9728b1051b000b1428c23aea19d5447ed1
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-01-05 16:36:34 +0100

    install aws cli with unzipped package

--- installawscli.sh
+++ installawscli.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 apt-get update
-apt-get install -y python-pip python-dev
-pip install awscli
+apt-get install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 echo "test S3 upload" > myfile.txt
