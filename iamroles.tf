c64f7fab243a152a1ef8b1c45b830338aeae51da
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-03-03 10:34:07 +0100

    Configure iam role reseource

--- iamroles.tf
+++ iamroles.tf
@@ -1,14 +1,14 @@
 #Roles to access the AWS S3 Bucket
 resource "aws_iam_role" "s3-levelupbucket-role" {
   name               = var.ROLE_NAME
-  assume_role_policy = 
+  assume_role_policy = file("./policies/s3clientrole.json")
 }
 
 #Policy to attach the S3 Bucket Role
 resource "aws_iam_role_policy" "s3-levelupmybucket-role-policy" {
   name = var.ROLE_POLICY_NAME
   role = aws_iam_role.s3-levelupbucket-role.id
-  policy = 
+  policy = file("./policies/s3accesspolicy.json")
 }
 
 #Instance identifier
