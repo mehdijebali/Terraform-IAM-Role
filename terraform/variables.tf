e6d1b919258d518c9b1220a356b6f3c27c71890f
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2025-09-13 21:11:00 +0200

    ✨ Feat/Add vars descriptions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,49 +1,59 @@
 variable "AWS_REGION" {
-  type    = string
-  default = "us-east-1"
+  type        = string
+  description = "The region where resources are deployed"
+  default     = "us-east-1"
 }
 
 variable "KEY_NAME" {
-  type    = string
-  default = "levelup_key"
+  type        = string
+  description = "SSH key name"
+  default     = "levelup_key"
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
-  type    = string
-  default = "levelup_key.pub"
+  type        = string
+  description = "Path to SSH public key"
+  default     = "levelup_key.pub"
 }
 
 variable "INSTANCE_TYPE" {
-  type    = string
-  default = "t2.micro"
+  type        = string
+  description = "AWS Instance type"
+  default     = "t2.micro"
 }
 
 variable "AVAILABILITY_ZONE" {
-  type    = string
-  default = "us-east-1a"
+  type        = string
+  description = "Availability Zone where instances is deployed"
+  default     = "us-east-1a"
 }
 
 variable "ROLE_NAME" {
-  type    = string
-  default = "s3-levelupbucket-role"
+  type        = string
+  description = "S3 Role Name"
+  default     = "s3-levelupbucket-role"
 }
 
 variable "ROLE_POLICY_NAME" {
-  type    = string
-  default = "s3-levelupmybucket-role-policy"
+  type        = string
+  description = "S3 Policy Name"
+  default     = "s3-levelupmybucket-role-policy"
 }
 
 variable "INSTANCE_NAME" {
-  type    = string
-  default = "tf_instance"
+  type        = string
+  description = "EC2 Instance Name"
+  default     = "tf_instance"
 }
 
 variable "SG_NAME" {
-  type    = string
-  default = "Allow_SSH"
+  type        = string
+  description = "Security Group Name"
+  default     = "Allow_SSH"
 }
 
 variable "SG_DESCRIPTION" {
-  type    = string
-  default = "Allow SSH Connection"
+  type        = string
+  description = "Security Group Description"
+  default     = "Allow SSH Connection"
 }
