806557e72c88d0b5aaa1605e52e557504e35f35a
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2025-09-14 12:24:42 +0200

    ✨ Feat/Add default tags

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,10 @@
 locals {
   S3_BUCKET_NAME = "demo-terraform-bucket-mj"
-
   s3-tags = {
     Name = local.S3_BUCKET_NAME
+  }
+  default-tags = {
+    Stack       = "IAM-ROLE",
+    Environment = "Production"
   }
 }
