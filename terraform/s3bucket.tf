fc62878927e264b15dfd9e492ad6dab9e024a189
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2025-10-28 11:13:18 +0100

    ⚡️ Feat/Enable s3 bucket versionning

--- terraform/s3bucket.tf
+++ terraform/s3bucket.tf
@@ -2,7 +2,13 @@
 
 resource "aws_s3_bucket" "levelup-s3bucket" {
   bucket = local.S3_BUCKET_NAME
-
+  
   tags = local.s3-tags
 }
 
+resource "aws_s3_bucket_versioning" "levelup-s3bucketversioning" {
+  bucket = aws_s3_bucket.levelup-s3bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
